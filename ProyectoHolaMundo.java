fdcec90166b5403a0625ac9e361874d0bd918ef0
GusDev <yael.gustavolop@gmail.com>
2026-05-08 12:41:51 -0600

    Adding 2 numbers

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,7 +1,15 @@
 public class ProyectoHolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-        System.out.println("");
-        System.out.println("Y a GitHub");
+        System.out.println("Bienvenido Java");
+        System.out.println("Y a Git/GitHub");
+
+        int numero1 = 5;
+        int numero2 = 5;
+
+        int suma = numero1 + numero2;
+
+        System.out.println(suma);
+
     }
 }
